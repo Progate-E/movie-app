fdbe3244523a9b344d6f47b1631ba216b74b85dd
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-06-25 14:44:11 +0700

    feat: add functions to fetch data from TMDB

---
+++ src/lib/fetch.ts
@@ -0,0 +1,51 @@
+import { Genre, MovieDetail, Movies } from '../global/types'
+
+async function fetchTMDB<T>(path: string, method = 'GET') {
+  console.log(`${process.env.EXPO_PUBLIC_TMDB_API_BASE_URL}${path}`)
+  return fetch(`${process.env.EXPO_PUBLIC_TMDB_API_BASE_URL}${path}`, {
+    method: method,
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.EXPO_PUBLIC_TMDB_API_KEY}`,
+    },
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error(`${res.status} (${res.statusText})`)
+    }
+    return res.json() as Promise<T>
+  })
+}
+
+const fetchPopularMovies = async (page: number): Promise<Movies> => {
+  return fetchTMDB(`/movie/popular?page=${page}`)
+}
+
+const fetchMovieDetail = async (movie_id: number): Promise<MovieDetail> => {
+  return fetchTMDB(`/movie/${movie_id}`)
+}
+
+const fetchAvailableMovieGenres = async (): Promise<Genre[]> => {
+  return fetchTMDB('/genre/movie/list')
+}
+
+const fetchMoviesByGenre = async (
+  genres: number[] | number,
+): Promise<Movies> => {
+  return fetchTMDB(
+    `/discover/movie?${new URLSearchParams({ with_genres: genres.toString() }).toString()}`,
+  )
+}
+
+const fetchMoviesByTitle = async (title: string): Promise<Movies> => {
+  return fetchTMDB(
+    `/search/movie?${new URLSearchParams({ query: title }).toString()}`,
+  )
+}
+
+export {
+  fetchAvailableMovieGenres,
+  fetchMovieDetail,
+  fetchMoviesByGenre,
+  fetchMoviesByTitle,
+  fetchPopularMovies,
+}

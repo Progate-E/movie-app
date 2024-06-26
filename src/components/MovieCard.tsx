ad7c7e08a34b5ab7eb865c18dac3dc39630cc5d6
whatsmynameidontknow <44807964+whatsmynameidontknow@users.noreply.github.com>
2024-06-26 21:48:57 +0700

    style: wrap styles object (#8)

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,7 +1,7 @@
 import { FontAwesome } from '@expo/vector-icons'
 import { StackActions, useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { DimensionValue, ImageBackground, View } from 'react-native'
+import { DimensionValue, ImageBackground, StyleSheet, View } from 'react-native'
 import { Card, Text } from 'react-native-paper'
 
 interface MovieCardProps {
@@ -34,78 +34,34 @@ export default function MovieCard(props: MovieCardProps): React.ReactElement {
         )
       }}
       mode="elevated"
-      style={{
-        width: props.width || (props.landscape ? 250 : 150),
-        height: props.height || (props.landscape ? 175 : 200),
-        margin: 5,
-      }}
+      style={[
+        styles.m5,
+        props.landscape ? styles.landscape : styles.portrait,
+        props.width && { width: props.width },
+        props.height && { height: props.height },
+      ]}
     >
       <ImageBackground
         source={{
-          uri: `https://image.tmdb.org/t/p/w300${props.landscape ? props.poster_path : props.backdrop_path}`,
+          uri: `https://image.tmdb.org/t/p/w300${props.landscape ? props.backdrop_path : props.poster_path}`,
         }}
-        style={{
-          width: '100%',
-          height: '100%',
-        }}
+        style={styles.imageBackground}
       >
-        <View
-          style={{
-            backgroundColor: 'rgba(0,0,0,0.5)',
-            position: 'absolute',
-            bottom: 0,
-            width: '100%',
-          }}
-        >
-          <Card.Title
-            title={props.title}
-            titleStyle={{
-              color: 'white',
-              marginBottom: 0,
-              fontWeight: '900',
-            }}
-            style={{
-              padding: 0,
-              margin: 0,
-            }}
-          />
-          <Card.Content
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              paddingTop: 0,
-              paddingBottom: 5,
-            }}
-          >
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'flex-start',
-              }}
-            >
+        <View style={styles.cardData}>
+          <Card.Title title={props.title} titleStyle={styles.cardTitle} />
+          <Card.Content style={styles.cardContent}>
+            <View style={styles.rating}>
               <FontAwesome
                 name="star"
                 color={'gold'}
                 size={20}
-                style={{ marginRight: 10 }}
+                style={styles.starIcon}
               />
-              <Text
-                style={{
-                  fontSize: 15,
-                  fontWeight: '900',
-                  color: 'gold',
-                }}
-              >
+              <Text style={styles.ratingText}>
                 {props.vote_average.toPrecision(2)}
               </Text>
             </View>
-            <Text
-              style={{
-                color: 'white',
-                fontSize: 15,
-                fontWeight: '900',
-              }}
-            >
+            <Text style={styles.releaseYear}>
               {new Date(props.release_date).getFullYear()}
             </Text>
           </Card.Content>
@@ -114,3 +70,54 @@ export default function MovieCard(props: MovieCardProps): React.ReactElement {
     </Card>
   )
 }
+
+const styles = StyleSheet.create({
+  portrait: {
+    width: 150,
+    height: 200,
+  },
+  landscape: {
+    width: 250,
+    height: 175,
+  },
+  m5: {
+    margin: 5,
+  },
+  imageBackground: {
+    width: '100%',
+    height: '100%',
+  },
+  cardData: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  },
+  cardTitle: {
+    color: 'white',
+    marginBottom: 0,
+    fontWeight: '900',
+  },
+  cardContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 5,
+  },
+  rating: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+  starIcon: {
+    marginRight: 5,
+  },
+  ratingText: {
+    color: 'gold',
+    fontSize: 15,
+    fontWeight: '900',
+  },
+  releaseYear: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: '900',
+  },
+})
--- src/util/styles.ts
+++
@@ -1,9 +0,0 @@
-import { Platform, StatusBar, StyleSheet } from 'react-native'
-
-const styles = StyleSheet.create({
-  NotchHelper: {
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
-})
-
-export default styles

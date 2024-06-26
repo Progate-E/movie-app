d7f0de3e4002e2f35ebc62437b5b6353ed1c64b7
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-06-26 15:34:14 +0700

    feat: add home screen

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,115 @@
+import { FontAwesome } from '@expo/vector-icons'
+import { StackActions, useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { DimensionValue, ImageBackground, View } from 'react-native'
+import { Card, Text } from 'react-native-paper'
+
+interface MovieCardProps {
+  id: number
+  title: string
+  poster_path: string
+  popularity: number
+  vote_average: number
+  overview: string
+  release_date: string
+  backdrop_path: string
+  landscape?: boolean
+  width?: DimensionValue
+  height?: DimensionValue
+  unPressable?: boolean
+}
+
+export default function MovieCard(props: MovieCardProps): React.ReactElement {
+  const navigation = useNavigation()
+
+  return (
+    <Card
+      onPress={() => {
+        if (props.unPressable) return
+        navigation.dispatch(
+          StackActions.push('Detail', {
+            id: props.id,
+          }),
+        )
+      }}
+      mode="elevated"
+      style={{
+        width: props.width || (props.landscape ? 250 : 150),
+        height: props.height || (props.landscape ? 175 : 200),
+        margin: 5,
+      }}
+    >
+      <ImageBackground
+        source={{
+          uri: `https://image.tmdb.org/t/p/w300${props.landscape ? props.poster_path : props.backdrop_path}`,
+        }}
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: 'rgba(0,0,0,0.5)',
+            position: 'absolute',
+            bottom: 0,
+            width: '100%',
+          }}
+        >
+          <Card.Title
+            title={props.title}
+            titleStyle={{
+              color: 'white',
+              marginBottom: 0,
+              fontWeight: '900',
+            }}
+            style={{
+              padding: 0,
+              margin: 0,
+            }}
+          />
+          <Card.Content
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingTop: 0,
+              paddingBottom: 5,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+              }}
+            >
+              <FontAwesome
+                name="star"
+                color={'gold'}
+                size={20}
+                style={{ marginRight: 10 }}
+              />
+              <Text
+                style={{
+                  fontSize: 15,
+                  fontWeight: '900',
+                  color: 'gold',
+                }}
+              >
+                {props.vote_average.toPrecision(2)}
+              </Text>
+            </View>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 15,
+                fontWeight: '900',
+              }}
+            >
+              {new Date(props.release_date).getFullYear()}
+            </Text>
+          </Card.Content>
+        </View>
+      </ImageBackground>
+    </Card>
+  )
+}
--- src/screens/Detail.tsx
+++ src/screens/Detail.tsx
@@ -1,24 +1,130 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { ParamListBase } from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import React, { useEffect, useState } from 'react'
+import { ScrollView, View } from 'react-native'
+import { ActivityIndicator, Divider, Text } from 'react-native-paper'
+import MovieCard from '../components/MovieCard'
+import MovieList from '../components/MovieList'
+import { MovieDetail } from '../global/types'
+import { fetchMovieDetail, fetchRecommendedMovies } from '../lib/fetch'
 
-export default function Detail(): React.ReactElement {
+interface DetailProps {
+  id: number
+}
+
+export default function Detail({
+  route,
+}: NativeStackScreenProps<ParamListBase>): React.ReactElement {
+  const [movie, setMovie] = useState<MovieDetail>({} as MovieDetail)
+  const [loading, setLoading] = useState<boolean>(true)
+  const { id } = route.params as DetailProps
+  useEffect(() => {
+    fetchMovieDetail(id)
+      .then((data) => {
+        setMovie(data)
+        setLoading(false)
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+  }, [])
   return (
     <View
       style={{
-        display: 'flex',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        width: '100%',
       }}
     >
-      <Text
-        style={{
-          fontSize: 50,
-          fontWeight: '900',
-        }}
-      >
-        Detail!!
-      </Text>
+      {loading ? (
+        <ActivityIndicator animating={true} size="large" />
+      ) : (
+        <ScrollView>
+          <MovieCard
+            width="100%"
+            height={250}
+            landscape
+            unPressable
+            {...movie}
+          />
+          <View
+            style={{
+              paddingHorizontal: 10,
+            }}
+          >
+            <Text variant="bodyMedium">{movie.overview}</Text>
+            <Divider
+              style={{
+                marginVertical: 5,
+              }}
+            />
+            <View
+              style={{
+                flexDirection: 'row',
+              }}
+            >
+              <Text
+                variant="labelLarge"
+                style={{
+                  fontWeight: '900',
+                }}
+              >
+                Release Date:{' '}
+              </Text>
+              <Text variant="bodyMedium">
+                {new Date(movie.release_date).toLocaleDateString('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric',
+                })}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+              }}
+            >
+              <Text
+                variant="labelLarge"
+                style={{
+                  fontWeight: '900',
+                }}
+              >
+                {movie.spoken_languages.length > 1
+                  ? 'Languages: '
+                  : 'Language: '}
+              </Text>
+              <Text variant="bodyMedium">
+                {movie.spoken_languages
+                  .map((lang) => lang.english_name)
+                  .join(', ')}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+              }}
+            >
+              <Text
+                variant="labelLarge"
+                style={{
+                  fontWeight: '900',
+                }}
+              >
+                Rating:{' '}
+              </Text>
+              <Text variant="bodyMedium">
+                {`${movie.vote_average.toPrecision(2)} (${movie.vote_count} votes)`}
+              </Text>
+            </View>
+          </View>
+          <MovieList
+            title="You may also like"
+            fetchFunc={async () => fetchRecommendedMovies(movie.id)}
+            onSeeMore={() => {
+              console.log('See More')
+            }}
+          />
+        </ScrollView>
+      )}
     </View>
   )
 }
